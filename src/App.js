b6bc935fbc91d5788ceffd54273672e8f98357a5
gilaguilar <redeyedev.208@gmail.com>
2024-02-04 13:35:27 -0700

    commit with first tests for testing strategy notes when needed

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img
+          src={logo}
+          className='App-logo'
+          alt='logo'
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://www.deque.com/'
+          target='_blank'
+          rel='noopener noreferrer'
+          data-testid='learn-link'
         >
-          Learn React
+          Learn web accessibility with Deque and how to run tests with jest.
         </a>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app with link without crashing', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByTestId('learn-link');
   expect(linkElement).toBeInTheDocument();
+});
+
+test('url is correct', () => {
+  render(<App />);
+
+  const linkElement = screen.getByTestId('learn-link');
+  expect(linkElement.href).toContain('https://www.deque.com/');
 });
